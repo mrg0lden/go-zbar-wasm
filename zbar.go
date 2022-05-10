de21e5fb1b9f57e875230e205be46fb5c19d262a
MrG0lden <8347078+mrg0lden@users.noreply.github.com>
2022-05-10 06:49:01 +0300

    add: global pool with convenient functions

--- zbar.go
+++ zbar.go
@@ -2,6 +2,8 @@ package zbar
 
 import (
 	"context"
+	"image"
+	"sync"
 
 	_ "embed"
 
@@ -15,6 +17,7 @@ var (
 	zbarCompiled wazero.CompiledCode
 	ctx          = context.Background()
 	r            = wazero.NewRuntimeWithConfig(wazero.NewRuntimeConfigJIT())
+	pool         = sync.Pool{New: func() any { return NewScanner() }}
 )
 
 func init() {
@@ -23,6 +26,20 @@ func init() {
 		ExportFunction("emscripten_notify_memory_growth", func(int32) {}).
 		Instantiate(ctx))
 	zbarCompiled = must(r.CompileModule(ctx, zbarWasm))
+}
+
+func ReadAll(img image.Image) ([][]byte, error) {
+	s := pool.Get().(*Scanner)
+	defer pool.Put(s)
+
+	return s.ReadAll(img)
+}
+
+func Read(img image.Image) (*Reader, error) {
+	s := pool.Get().(*Scanner)
+	defer pool.Put(s)
+
+	return s.Reader(img)
 }
 
 func must[T any](v T, err error) T {
